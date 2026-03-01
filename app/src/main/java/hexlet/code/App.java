3e25d331234028a68de7cfda831283e842b5444c
Iurii Rychkov <yuri.rychkov@gmail.com>
2026-03-01 16:32:24 +0100

    Fix app port

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,14 +5,18 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public final class App {
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "8080");
+        return Integer.valueOf(port);
+    }
 
-    public Javalin getApp() {
+    public static Javalin getApp() {
         return Javalin.create(config -> {
             config.routes.get("/", ctx -> ctx.result("Hello World"));
         });
     }
 
     public static void main(String[] args) {
-        new App().getApp().start();
+        getApp().start(getPort());
     }
 }
